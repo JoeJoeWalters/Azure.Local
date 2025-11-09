70de50bd35741e44a0be004af1723d79c1360d21
Joe Walters <info@thenakeddeveloper.com>
2025-11-09 21:40:35 +0000

    Added in application handler layer

--- src/application/Azure.Local.Application/Timesheets/TimesheetApplication.cs
+++ src/application/Azure.Local.Application/Timesheets/TimesheetApplication.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Azure.Local.Infrastructure.Repository;
+using Azure.Local.Infrastructure.Timesheets;
 
 namespace Azure.Local.Application.Timesheets
 {
     public class TimesheetApplication : ITimesheetApplication
     {
+        private readonly IRepository<TimesheetRepositoryItem> _repository;
+
+        public TimesheetApplication(IRepository<TimesheetRepositoryItem> repository)
+        {
+            _repository = repository;
+        }
     }
 }
