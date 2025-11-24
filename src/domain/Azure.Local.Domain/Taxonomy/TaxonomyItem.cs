f5bcc8f37c5483a04cabd6088fa5811c0aeb2df0
Joe Walters <info@thenakeddeveloper.com>
2025-11-24 23:49:38 +0000

    Added the concept of taxonomies for timesheet codes

---
+++ src/domain/Azure.Local.Domain/Taxonomy/TaxonomyItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Azure.Local.Domain.Taxonomy
+{
+    public class TaxonomyItem
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public required string Name { get; set; }
+        public string? ParentId { get; set; }
+    }
+}
