c8eb2d84f171e5ba6dab814a2e4aa9051e04845c
Joe Walters <info@thenakeddeveloper.com>
2025-12-02 00:13:50 +0000

    Refactor test projects and add test automation script
    Replaced `Azure.Local.ApiService.Tests` with `Azure.Local.Tests` to consolidate test projects. Updated the solution file to reflect this change. Added `<GenerateDocumentationFile>` to `Azure.Local.ApiService.csproj` to enable XML documentation generation.
    
    Introduced `tests.bat` script to automate testing and code coverage reporting. The script installs necessary tools, runs tests with coverage, generates HTML reports, and opens the results in the browser.
    
    Improved project organization and streamlined testing workflows.

---
+++ utils/tests.bat
@@ -0,0 +1,17 @@
+:: Echo %1
+dotnet tool install -g dotnet-reportgenerator-globaltool
+rd coverage /S /Q
+cd ..
+FOR /D /r %%G in ("tests/*.Tests") DO (
+	Echo We found %%~nxG
+	cd %%~nxG
+	dir /b /a-d
+	rd TestResults /S /Q
+	dotnet test --collect:"XPlat Code Coverage"
+	cd ..
+)
+cd /D "%~dp0"
+reportgenerator "-reports:../tests/**/coverage.cobertura.xml" "-targetdir:./coverage" "-reporttypes:Html"
+start ./coverage/index.html
+
+pause
