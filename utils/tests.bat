e864d16330b8cb9e4eafbcc110a4737f6a78ef47
Walters, Joe <joe.walters@newday.co.uk>
2025-12-25 18:27:21 +0000

    Update test coverage to exclude generated code
    Refactor test and coverage workflow to use a custom codecoverage.runsettings file, excluding auto-generated code from coverage reports. Clean up old test results before running tests and improve script output for easier debugging. Removes legacy xUnit settings and ensures more accurate coverage metrics.

--- utils/tests.bat
+++ utils/tests.bat
@@ -2,18 +2,25 @@
 dotnet tool install -g dotnet-reportgenerator-globaltool
 rd coverage /S /Q
 cd ..
+
+:: Clean up old test results
+FOR /D /r %%G in (TestResults) DO (
+	Echo We found %%G
+	rd /S /Q %%G
+)
+
+:: Find any tests to run against to collect coverage
 FOR /D /r %%G in ("tests/*.Tests") DO (
-	Echo We found %%~nxG
-	cd %%~nxG
-	dir /b /a-d
-	rd TestResults /S /Q
-	dotnet test --collect:"XPlat Code Coverage"
+	Echo We found %%G
+	cd %%G
+	Echo Moved into directory %%G
+	:: https://rjj-software.co.uk/blog/excluding-auto-generated-code-from-net-10-coverage-reports/
+	dotnet test --collect:"XPlat Code Coverage" --settings:./codecoverage.runsettings
 	cd ..
 )
 
 :: Retarget back to the current directory
 cd /D "%~dp0"
-
 reportgenerator "-reports:../tests/**/coverage.cobertura.xml" "-targetdir:./coverage" "-reporttypes:Html"
 start ./coverage/index.html
 
